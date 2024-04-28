d7c4363e73cdc61d646326d3334777b3f67afb5c
Marouan Ajtirah <marouan_39@hotmail.com>
2024-04-28 17:29:27 +0200

    first

---
+++ src/Entity/Movie.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MovieRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=MovieRepository::class)
+ */
+class Movie
+{
+    /**
+     * @ORM\Id()
+     *
+     * @ORM\GeneratedValue()
+     *
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $director;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $ReleaseYear;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $description;
+
+    // Getters and setters
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getDirector(): ?string
+    {
+        return $this->director;
+    }
+
+    public function setDirector(string $director): self
+    {
+        $this->director = $director;
+
+        return $this;
+    }
+
+    public function getYearOfRelease(): ?int
+    {
+        return $this->ReleaseYear;
+    }
+
+    public function setYearOfRelease(int $ReleaseYear): self
+    {
+        $this->ReleaseYear = $ReleaseYear;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+}
